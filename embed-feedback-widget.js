b01a131f470614326389627e72e0df2dff3b6003
Yashpal Singh <yashpalsingh9rx@outlook.com>
2024-10-05 17:32:32 +0530

    add

--- embed-feedback-widget.js
+++ embed-feedback-widget.js
@@ -1,16 +1,75 @@
-(function() {
-  function createFeedbackWidget() {
-    const iframe = document.createElement('iframe');
-    iframe.src = `https://yshplsngh.github.io/cdn/feedback-widget.html`;
-    iframe.style.position = 'fixed';
-    iframe.style.bottom = '15px';
-    iframe.style.right = '15px';
-    iframe.style.width = '300px';
-    iframe.style.height = '200px';
-    iframe.style.border = 'none';
-    iframe.style.boxShadow = '0 2px 10px rgba(0,0,0,0.2)';
-    document.body.appendChild(iframe);
-  }
+class FeedbackWidget extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({mode: 'open'});
+	}
 
-  window.createFeedbackWidget = createFeedbackWidget;
-})();
+	connectedCallback() {
+		const spaceName = this.getAttribute('spaceName') || '';
+
+		this.render(spaceName);
+		this.fetchFeedbacks(spaceName).then((feedbacks) => {
+			if (feedbacks) {  // Add a check here
+				this.displayFeedbacks(feedbacks);
+			}
+		});
+	}
+
+	render(spaceName) {
+		const styles = ` 
+            :host {
+                font-family: Arial, sans-serif;
+            }
+            .widget {
+                position: fixed;
+                bottom: 15px;
+                right: 15px;
+                width: 300px;
+                padding: 10px;
+                border-radius: 5px;
+                box-shadow: 0 2px 10px rgba(0,0,0,0.2);
+                background-color: #f0f0f0;
+                color: #333;
+            }
+            #feedbacks {
+                margin-top: 10px;
+                max-height: 150px;
+                overflow-y: auto;
+            }
+        `;
+
+		this.shadowRoot.innerHTML = `
+            <style>${styles}</style>
+            <div class="widget">
+                <h1>${spaceName}</h1>
+                <div id="feedbacks"></div>
+            </div>
+        `;
+	}
+
+	async fetchFeedbacks(spaceName) {
+		try {
+			const response = await fetch(`http://localhost:4000/api/feedback/getFeedbacks/${spaceName}`);
+			if (response.ok) {
+				return await response.json();
+			} else {
+				throw new Error('Failed to fetch feedbacks');
+			}
+		} catch (error) {
+			console.error('Error fetching feedbacks:', error);
+			return null;  // Return null on error
+		}
+	}
+
+	displayFeedbacks(feedbacks) {
+		const feedbacksContainer = this.shadowRoot.querySelector('#feedbacks');
+		feedbacksContainer.innerHTML = '<h4>Recent Feedback:</h4>';
+		feedbacks.forEach(feedback => {
+			const feedbackElement = document.createElement('p');
+			feedbackElement.textContent = feedback.customerFeedback;
+			feedbacksContainer.appendChild(feedbackElement);
+		});
+	}
+}
+
+customElements.define('feedback-widget', FeedbackWidget);
